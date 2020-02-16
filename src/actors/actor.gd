af08defd3ae69e2a02f6fe9789906c7755d4a240
julianamarques <julianabmarques@live.com>
2020-02-16 12:58:35 -0300

    First tutorial part

---
+++ src/actors/actor.gd
@@ -0,0 +1,8 @@
+extends KinematicBody2D
+class_name Actor
+
+
+var _velocity: = Vector2.ZERO
+export var speed: = Vector2(300.0, 1000.0)
+export var gravity: = 3000.0
+const FLOOR_NORMAL: = Vector2.UP
